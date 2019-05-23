a77506b660ca435ba14f63c1b810130f3a7e405a
Daniel Tolbert <danielchristopher19@gmail.com>
2019-05-22 20:39:24 -0400

    Created forms cycle, fixed random, fixed method structure

--- Graph Theory/src/Dot.java
+++ Graph Theory/src/Dot.java
@@ -1,10 +1,13 @@
 import processing.core.PImage;
 
+import java.util.ArrayList;
+
 public class Dot {
 
     private PImage image;
     private int x;
     private int y;
+    private ArrayList<Edge> connectedEdges = new ArrayList<>(  );
 
     public Dot ( PImage image, int x, int y) {
         this.image = image;
@@ -18,6 +21,18 @@ public class Dot {
 
     public int getY() {
         return y;
+    }
+
+    public boolean isConnected() {
+        return connectedEdges.size() > 0;
+    }
+
+    public ArrayList<Edge> getConnected() {
+        return connectedEdges;
+    }
+
+    public void addConnected(Edge edge) {
+        connectedEdges.add( edge );
     }
 
     public PImage getImage() {
--- Graph Theory/src/Edge.java
+++ Graph Theory/src/Edge.java
@@ -2,10 +2,21 @@ public class Edge {
 
     private Dot a;
     private Dot b;
+    private int fill;
+    private String name;
 
     public Edge(Dot a, Dot b) {
+        this(a, b, 102);
+    }
+
+    public Edge(Dot a, Dot b, int fill) {
         this.a = a;
         this.b = b;
+        this.name = hashCode() + "";
+        this.fill = fill;
+
+        a.addConnected( this );
+        b.addConnected( this );
     }
 
     public Dot getConnectingDotA() {
@@ -16,8 +27,20 @@ public class Edge {
         return b;
     }
 
+    public int getFill() {
+        return fill;
+    }
+
+    public void setFill(int fill) {
+        this.fill = fill;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     public boolean equals(Edge otherEdge) {
-        return a == otherEdge.getConnectingDotA() && b == otherEdge.getConnectingDotB();
+        return (a == otherEdge.getConnectingDotA() && b == otherEdge.getConnectingDotB() ) || (a == otherEdge.getConnectingDotB() && b == otherEdge.getConnectingDotA());
     }
 
 
