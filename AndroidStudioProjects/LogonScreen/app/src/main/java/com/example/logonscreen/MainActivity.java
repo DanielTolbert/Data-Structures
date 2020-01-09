ebb759916f4aca04a7da1d938fc2ceefb70badc4
Daniel Tolbert <danielchristopher19@gmail.com>
2020-01-09 12:31:28 -0500

    Need to create logic for password matching

--- AndroidStudioProjects/LogonScreen/app/src/main/java/com/example/logonscreen/MainActivity.java
+++ AndroidStudioProjects/LogonScreen/app/src/main/java/com/example/logonscreen/MainActivity.java
@@ -2,13 +2,62 @@ package com.example.logonscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+
+    private Button signup;
+    private Button login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createViews();
+    }
+
+    private void createViews() {
+        username = findViewById(R.id.editTextUsername);
+        password = findViewById(R.id.editTextPassword);
+
+        login = findViewById(R.id.buttonLogIn);
+        signup = findViewById(R.id.buttonMakeAccount);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ( validatePassword() ){
+                    validateAccountExistence();
+                }
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAccountActivity();
+            }
+        });
+    }
+
+    private boolean validatePassword() {
+        return false;
+    }
+
+    private void validateAccountExistence() {
+
+    }
+
+    private void createAccountActivity() {
+        Intent intent = new Intent(getBaseContext(), SignupActivity.class);
+        startActivity(intent);
     }
 }
---
+++ AndroidStudioProjects/LogonScreen/app/src/main/java/com/example/logonscreen/SignupActivity.java
@@ -0,0 +1,55 @@
+package com.example.logonscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SignupActivity extends AppCompatActivity {
+
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private EditText editTextRe_password;
+    private Button buttonCreateAccount;
+
+    public SignupActivity() {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        createViews();
+    }
+
+    private void createViews() {
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextRe_password = findViewById(R.id.editTextReenterPassword);
+
+        buttonCreateAccount = findViewById(R.id.buttonMakeAccount);
+        buttonCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validatePassword()) {
+                    createAccount(editTextUsername.getText().toString(), editTextPassword.getText().toString());
+                }
+            }
+        });
+
+    }
+
+    private boolean validatePassword() {
+        boolean matchingPasswords = editTextPassword.getText().toString().equals( editTextUsername.getText().toString() );
+        return matchingPasswords;
+    }
+
+    private void createAccount(String user, String pass) {
+
+    }
+
+
+}
